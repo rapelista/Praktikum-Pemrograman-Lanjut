8661390c3e4f13a4a18626976ad0e2385766bd11
Farih Akmal Haqiqi <farih.282828@gmail.com>
2022-09-26 18:09:22 +0700

    add src for 04

---
+++ 04/Prosedur Praktikum/Program2.py
@@ -0,0 +1,16 @@
+from os import link
+
+
+perusahaan = 'PRAKTIKUM PEMROGRAMAN LANJUT"
+list_jurusan = ['Teknik Komputer',
+                'Teknik Informatika', 'Magister Ilmu Komputer']
+
+mahasiswa = {
+    'nama': 'Asep Ranta Munajat',
+    'asal': 'Bekasi'
+}
+
+print(perusahaan[5])
+print(link_jurusan[7])
+print(perusahaan[-1])
+print(link_jurusan[-3])
